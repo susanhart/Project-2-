b0c771575c1a034bf25f592af0b62a6cae63524f
Sue <susanhartesquire@yahoo.com>
2019-05-07 11:16:33 -0400

    Commit
    Deleted instructions that came with the project

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -5,16 +5,7 @@ FSJS project 2 - List Filter and Pagination
 
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+
 //The list of variables (that store the DOM elements) needed to paginate.
  //Assigning ten students per page load.
 
@@ -22,20 +13,6 @@ const studentsPerPage = 10;
 const pageList = document.querySelector('ul');
 const eachStudent = pageList.children;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 // Function to determine the number of pages based on the number of students.
 
 function numberOfPages() {
@@ -136,4 +113,5 @@ showPage(eachStudent, 1);
 //Function call to show the six separate page links.
 appendPageLinks ()
 
+//Function call to show the search box for searching for a particular student from the list of 54 students.
 showSearch ()
